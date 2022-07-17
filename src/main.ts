9bde465ad3e874a489aed936d5310e569e9db149
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-07-17 21:29:54 +0200

    feat: swagger specification added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +16,12 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(process.env.PORT || 4000);
 }
 
