5fba742895a82c966df929d028d106ec01e323d8
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-07-31 21:36:37 +0200

    feat: typeorm migrations file added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { DbModule } from './db/db.module';
 import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './typeorm.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     UsersModule,
-    DbModule,
     ArtistsModule,
     TracksModule,
     AlbumsModule,
--- src/db/db.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InMemoryDB } from './db.service';
-
-@Module({
-  providers: [InMemoryDB],
-})
-export class DbModule {}
